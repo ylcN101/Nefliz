c5f959ccb6972271d39fcc889a620e75c6a99173
ylcN101 <yuvalcohen12234@gmail.com>
2023-05-28 16:52:17 +0300

    added auth

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,10 @@
+import type { AppProps } from 'next/app'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
   return <Component {...pageProps} />
 }
-
-export default MyApp
