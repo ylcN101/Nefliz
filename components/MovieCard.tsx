5a8445be516bf6c2af976886bbdbbaa73dd21972
ylcN101 <yuvalcohen12234@gmail.com>
2023-05-31 01:07:46 +0300

    added modal on mobile

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -14,7 +14,9 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const { openModal } = useInfoModal()
 
   return (
-    <div className="group bg-zinc-900 col-span relative h-[18vw] md:h-[12vw]">
+    <div
+      onClick={() => openModal(data?.id)}
+      className="group bg-zinc-900 col-span relative h-[18vw] md:h-[12vw]">
       <img
         className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[18vw] md:h-[12vw]"
         src={data.thumbnailUrl}
