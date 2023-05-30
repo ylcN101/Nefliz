fe5547b59df477e491f53549c9925e737c44efc1
ylcN101 <yuvalcohen12234@gmail.com>
2023-05-30 20:31:16 +0300

    fixed info

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { useRouter } from 'next/router'
 import { BsFillPlayFill } from 'react-icons/bs'
+import { BiChevronDown } from 'react-icons/bi'
 import FavoriteButton from './FavoriteButton'
 import useInfoModal from '@/hooks/useInfoModal'
 
@@ -33,6 +34,14 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
               <BsFillPlayFill size={30} />
             </div>
             <FavoriteButton movieId={data.id} />
+            <div
+              onClick={() => openModal(data?.id)}
+              className="cursor-pointer ml-auto group/item w-6 h-6 lg:w-10 lg:h-10 border-white border-2 rounded-full flex justify-center items-center transition hover:border-neutral-300">
+              <BiChevronDown
+                size={30}
+                className="text-white group-hover/item:text-neutral-300"
+              />
+            </div>
           </div>
           <p className="text-green-400 font font-semibold mt-4">
             New <span className="text-white">2023</span>
