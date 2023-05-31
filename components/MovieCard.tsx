2f9682612ebd9413d0adb97ac6b6a0f01879db29
ylcN101 <yuvalcohen12234@gmail.com>
2023-05-31 12:40:00 +0300

    fixed media player

--- components/MovieCard.tsx
+++ components/MovieCard.tsx
@@ -13,11 +13,16 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
   const router = useRouter()
   const { openModal } = useInfoModal()
 
+  const openPlayer = () => {
+    router.push(`
+      /watch/${data?.id}
+    `)
+  }
+
   return (
-    <div
-      onClick={() => openModal(data?.id)}
-      className="group bg-zinc-900 col-span relative h-[18vw] md:h-[12vw]">
+    <div className="group bg-zinc-900 col-span relative h-[18vw] md:h-[12vw]">
       <img
+        onClick={() => openModal(data?.id)}
         className="cursor-pointer object-cover transition duration shadow-xl rounded-md group-hover:opacity-90 sm:group-hover:opacity-0 delay-300 w-full h-[18vw] md:h-[12vw]"
         src={data.thumbnailUrl}
         alt="thumbnail"
@@ -31,7 +36,7 @@ const MovieCard: React.FC<MovieCardProps> = ({ data }) => {
         <div className="z-10 bg-zinc-800 p-2 lg:p-4 absolute w-full transition shadow-md rounded-b-md">
           <div className="flex flex-row items-center gap-3">
             <div
-              onClick={() => router.push(`/watch/${data?.id}`)}
+              onClick={openPlayer}
               className="cursor-pointer w-6 h-6 lg:w-10 lg:h-10 bg-white rounded-full flex justify-center items-center transition hover:bg-neutral-300">
               <BsFillPlayFill size={30} />
             </div>
